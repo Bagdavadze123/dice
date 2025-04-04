893c70fc1452cc870085b93b8a1bde0e9234f5ee
Zura bagdavadze <108548844+Bagdavadze123@users.noreply.github.com>
2025-04-05 01:07:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-var randomDiceImage = "images/dice" + randomNumber1 + ".png"; 
+var randomDiceImage = "./dice" + randomNumber1 + ".png"; 
 document.getElementsByClassName("img1")[0].setAttribute("src", randomDiceImage);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"; 
+var randomDiceImage2 = "./dice" + randomNumber2 + ".png"; 
 document.getElementsByClassName("img2")[0].setAttribute("src", randomDiceImage2);
 
 
